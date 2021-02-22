a43354e5ea9bf2bf83da0c482fb041896d69bdda
agilsae <agilsae.cs@gmail.com>
2021-02-22 16:12:56 +0700

    kumpulkan

--- index.js
+++ index.js
@@ -1,29 +1,17 @@
 var express = require('express');
 var app = express();
 
-var conn = require('express-myconnection');
-var mysql = require('mysql');
+var produk = require('./routes/produk');
 
-var perintahku = require('./routes/perintahku');
-
-app.set('port', process.env.port || 3777);
-app.use(
-    conn(mysql, {
-        host: 'localhost',
-        user: 'root',
-        password:'password',
-        port:'3306',
-        database:'ecommerce'
-    },'single')
-);
+app.set('port', process.env.port || 8000);
 
 app.listen(app.get('port'), function(){
     console.log('Server is running on port ' + app.get('port'));
 });
 
 app.get('/', function(req, res){
-    res.send('Server is running on port' + app.get('port'));
+    res.send('Ini adalah halaman Home web Agil');
 });
 
-app.get('/create_table', perintahku.createTableProduct);
-app.get('/insert_table', perintahku.insertTableProduct);
+app.get('/product', produk.product);
+app.get('/detailproduct', produk.detailProduct);
---
+++ routes/produk.js
@@ -0,0 +1,7 @@
+exports.product = function (req, res){
+       res.send('Ini adalah halaman produk web Agil');
+}
+
+exports.detailProduct = function (req, res){
+    res.send('Ini adalah halaman detail produk web Agil');
+}
